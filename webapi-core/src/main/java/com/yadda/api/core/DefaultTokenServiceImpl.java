a49a95ce92593808c0bc18cc3c8300060ec65fb1
yadda <silenceisok@163.com>
2017-10-31 18:23:44 +0800

    DefaultTokenServiceImpl

--- webapi-core/src/main/java/com/yadda/api/core/DefaultTokenServiceImpl.java
+++ webapi-core/src/main/java/com/yadda/api/core/DefaultTokenServiceImpl.java
@@ -2,19 +2,28 @@ package com.yadda.api.core;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yadda
+ * TokenService 默认实现
+ */
 @Service(value = "DefaultTokenServiceImpl")
 public class DefaultTokenServiceImpl implements TokenService {
 
-	@Override
-	public Token createToken() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private volatile Map<String, Object> tokens = new HashMap<>();
 
-	@Override
-	public Token getToken(String token) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Token createToken() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Token getToken(String token) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
